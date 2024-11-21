b7cfda199e6d0c0e84bbe18a809b2a96b214869f
Natoria Milligan <natoriaray@gmail.com>
2024-11-21 16:10:16 -0600

    Created template for job endpoints

--- resources/job.py
+++ resources/job.py
@@ -6,3 +6,31 @@ from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from db import db
 from models import JobModel
 
+blp = Blueprint("job", __name__, description="Operations on jobs")
+
+@blp.route("/job")
+class JobList(MethodView):
+    def get(self):
+        return JobModel.query.all()
+    
+    def post(self, job_data):
+        pass
+
+@blp.route("/job/<int:job_id>")
+class Job(MethodView):
+    def get(self, job_id):
+        pass
+
+    def put(self, job_id):
+        pass
+
+    def delete(self, job_id):
+        pass
+
+
+@blp.route("/employer/<int:employer_id>/job")
+class JobWithEmployer(MethodView):
+    def get(self, employer_id):
+        pass
+
+
