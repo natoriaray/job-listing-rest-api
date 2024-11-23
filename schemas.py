8b865aeedcb83e1924889e0722be55a94f7ff951
Natoria Milligan <natoriaray@gmail.com>
2024-11-23 10:18:54 -0600

    Added job schema and updated employer schema

--- schemas.py
+++ schemas.py
@@ -1,11 +1,27 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
+from marshmallow.validate import OneOf
+
+states = [
+    "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", 
+    "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", 
+    "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", 
+    "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", 
+    "WI", "WY"
+]
 
 class EmployerSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
 
 class JobSchema(Schema):
+    id = fields.Int(dump_only=True)
     title = fields.Str(required=True)
+    description = fields.Str()
+    salary = fields.Int()
+    city = fields.Str()
+    state = fields.Str(validate=OneOf(states))
+    employer_id = fields.Int(required=True, load_only=True)
+    employer = fields.List(fields.Nested(EmployerSchema), dump_only=True)
 
 class FullEmployerSchema(EmployerSchema):
     jobs = fields.List(fields.Nested(JobSchema), dump_only=True)
